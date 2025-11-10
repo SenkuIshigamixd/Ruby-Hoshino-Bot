580e1520c9b4275c87994bdda354908ce81b2972
Sifu <155864076+SenkuIshigamixd@users.noreply.github.com>
2025-11-09 23:03:17 -0600

    Update avatar URL in grupo-opciones.js

--- plugins/grupo-opciones.js
+++ plugins/grupo-opciones.js
@@ -7,7 +7,7 @@ const handler = async (m, { conn }) => {
 
   const primaryBot = chat.botPrimario ? `@${chat.botPrimario.split('@')[0]}` : 'Sin establecer';
 
-  const avatar = "https://files.catbox.moe/1k2k6p.jpg";
+  const avatar = "https://acortar.link/cbq3BT";
 
   const text = `╭━━━[ *CONFIGURACIÓN* ]━━━⬣
 ┃
